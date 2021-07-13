adb8ba56feb96918eb2e468ea4a49563439fc2c3
Akrobate <fedorov.artiom@gmail.com>
2021-07-13 13:47:32 +0200

    Fixed app probe app

--- probe-app/app.js
+++ probe-app/app.js
@@ -2,27 +2,43 @@
 
 const port = process.env.PROBE_APP_PORT ? process.env.PROBE_APP_PORT : 3000;
 
-const express = require('express');
-const app = express();
-app.use((request, response) => {
+require('express')()
+    .use(
+        (request, response) => {
+            const result = {
+                url: request.originalUrl,
+                host: request.headers.host,
+                env_vars: formatEnvVars(process.env),
+            }
+            const result_html = `<pre>${JSON.stringify(result, null, 2)}</pre>`;
+            console.log(`call: ${result.host} ${result.url}`);
+            response
+                .send(result_html)
+        }
+    )
+    .listen(
+        port,
+        (error) => {
+            if (error) {
+                console.log(error);
+                process.exit(1);
+            }
+            console.log(`probe app is listening on port ${port}`);
+    });
 
-    const result = {
-        url: request.originalUrl,
-        host: request.headers.host,
-        env_vars: process.env,
-    }
-    console.log(`call: ${result.host} ${result.url}`);
-    response
-        .status(200)
-        // .send(result);
-        .send(`<pre>${JSON.stringify(result, null, 2)}</pre>`)
-    }
-);
 
-app.listen(port, (error) => {
-    if (error) {
-        console.log(error);
-        process.exit(1);
-    }
-    console.log(`service-nylas is listening on port ${port}`);
-});
+function formatEnvVars(process_env) {
+    const npm_config = {};
+    const envs = {};
+    Object.keys(process_env).forEach((item) => {
+        if (item.includes('npm_config')) {
+            npm_config[item] = process_env[item];
+        } else {
+            envs[item] = process_env[item];
+        }
+    });
+    return {
+        envs,
+        npm_config,
+    };
+}
